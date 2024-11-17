b3d05d9cd22d7254dbff3bc8d1ce7e8604b2745c
Elijah Llegado <ea.leggy@gmail.com>
2024-11-17 11:10:53 -0800

    Fixed crashes caused by tick injection

--- src/main/java/net/legionoid/workingsincebirth/mixin/MixinVillagerEntity.java
+++ src/main/java/net/legionoid/workingsincebirth/mixin/MixinVillagerEntity.java
@@ -1,15 +1,20 @@
 package net.legionoid.workingsincebirth.mixin;
 
-import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EntityData;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.village.VillagerProfession;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.ServerWorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 import java.util.Random;
@@ -18,16 +23,35 @@ import java.util.Random;
 public abstract class MixinVillagerEntity {
     private static final Random RANDOM = new Random();
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void onSpawn(EntityType<? extends VillagerEntity> entityType, ServerWorld world, CallbackInfo ci) {
+    @Inject(method = "initialize", at = @At("RETURN"))
+    private void onInitialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, EntityData entityData, NbtCompound entityTag, CallbackInfoReturnable<EntityData> cir) {
         VillagerEntity villager = (VillagerEntity) (Object) this;
 
-        Registry<VillagerProfession> professionRegistry = world.getRegistryManager().get(RegistryKeys.VILLAGER_PROFESSION);
+        if (world instanceof ServerWorld serverWorld) {
+            Registry<VillagerProfession> professionRegistry = serverWorld.getRegistryManager().get(RegistryKeys.VILLAGER_PROFESSION);
+            List<VillagerProfession> professions = professionRegistry.stream().toList();
 
-        List<VillagerProfession> professions = professionRegistry.stream().toList();
+            VillagerProfession randomProfession = professions.get(RANDOM.nextInt(professions.size()));
 
-        VillagerProfession randomProfession = professions.get(RANDOM.nextInt(professions.size()));
+            villager.setVillagerData(villager.getVillagerData().withProfession(randomProfession));
+            villager.setExperience(1);
+        }
+    }
 
-        villager.setVillagerData(villager.getVillagerData().withProfession(randomProfession));
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void onTick(CallbackInfo ci) {
+        VillagerEntity villager = (VillagerEntity) (Object) this;
+
+        if (villager.getVillagerData().getProfession() == VillagerProfession.NONE) {
+            if (villager.getWorld() instanceof ServerWorld serverWorld) {
+                Registry<VillagerProfession> professionRegistry = serverWorld.getRegistryManager().get(RegistryKeys.VILLAGER_PROFESSION);
+
+                List<VillagerProfession> professions = professionRegistry.stream().toList();
+                VillagerProfession randomProfession = professions.get(RANDOM.nextInt(professions.size()));
+
+                villager.setVillagerData((villager.getVillagerData().withProfession(randomProfession)));
+                villager.setExperience(1);
+            }
+        }
     }
 }
